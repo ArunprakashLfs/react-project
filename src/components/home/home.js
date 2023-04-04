19bcd8a5073133a2fe95ed213d3d1eeef35e5e09
kesavan <arunprakash7871@gmail.com>
2023-04-04 06:20:43 -0400

    added css property

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -8,10 +8,12 @@ const Home =({Data})=>{
     console.log(data);
     return(
         <div className="card">
-            {data.map(({title, images})=>{
+            {data.map(({title, images, id, brand, description})=>{
             return (
                 <div >
-                    <h3>{title}</h3>
+                    <h3 key={id}>{title}</h3>
+                    <h4>{brand}</h4>
+                    <p>{description}</p>
                     {/* {console.log(image)} */}
                     <div className="image-container">
                     <img src={images[0]} alt={title} className="img"></img>

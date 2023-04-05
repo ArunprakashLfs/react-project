2cf4ab901968d516ab5f631f21a97b0e96eecde6
kesavan <arunprakash7871@gmail.com>
2023-04-05 06:04:25 -0400

    add logo image

--- src/components/navbarContainer/navbarContainer.js
+++ src/components/navbarContainer/navbarContainer.js
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 import './navbar.styles.scss';
+import logo from '../assets/logo192.png'
 
 const NavbarContainer = ()=>{
     return(
         <nav className="link-container">
             <div className="symbol">
-                React
+                react
+                <img src={logo} alt="logo" className="logo-img"></img>
             </div>
             <div className="link"> 
                 <Link to={'/'}>home</Link>
                 <Link to={'/Shop'}>shop</Link>
-                <Link to={'/signin'}>signin</Link>
+                <Link to={'/signin'}>users</Link>
             </div>
             
         </nav>

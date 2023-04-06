0aa73092bfaf709b37004e7be91be06e816895c5
kesavan <arunprakash7871@gmail.com>
2023-04-06 02:03:45 -0400

    changed color proprty in navbar

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import NavbarContainer from './components/navbarContainer/navbarContainer';
 import { Outlet } from 'react-router-dom';
 import Home from './components/home/home';
 import Shop from './components/shop/shop';
-import Signin from './components/signincomponents/signin'
+import Signin from './components/signincomponents/signin';
+import logo from './components/assets/logo192.png';
 
 function App() {
   return(
     <Routes>
       <Route path={'/'}
-      element={<><NavbarContainer /><Outlet/></>}
+      element={<><NavbarContainer logo={logo}/><Outlet/></>}
       >
         <Route index
         element={<Home Data={Data}/>}></Route>
--- src/components/navbarContainer/navbarContainer.js
+++ src/components/navbarContainer/navbarContainer.js
@@ -5,12 +5,12 @@ import logo from '../assets/logo192.png'
 const NavbarContainer = ()=>{
     return(
         <nav className="link-container">
-            <div className="symbol">
+            <Link className="symbol"  to={'/'}>
                 <span>React</span>
                 <img src={logo} alt="logo" className="logo-img"></img>
-            </div>
+            </Link>
             <div  className="link-component"> 
-                <Link className="link" to={'/'}>home</Link>
+                {/* <Link className="link" to={'/'}>home</Link> */}
                 <Link className="link" to={'/Shop'}>shop</Link>
                 <Link className="link" to={'/signin'}>users</Link>
             </div>
